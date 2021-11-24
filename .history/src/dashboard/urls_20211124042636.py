4842f948d3bc782d7201f03ebcacc814de6adbfa
jackkweyunga <jackkweyunga@gmail.com>
2021-11-24 05:27:53 +0300

    finished working on services-dashboard

---
+++ .history/src/dashboard/urls_20211124042636.py
@@ -0,0 +1,15 @@
+from django.urls import path
+import dashboard.views as views
+
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name="dashboard"),
+    path('emails/', views.EmailsView.as_view(), name="emails-dashboard"),
+    path('users/', views.UsersView.as_view(), name="users-dashboard"),
+    
+    # service paths/urls
+    path('services/', views.ServicesView.as_view(), name="services-dashboard"),
+    path('services/add', views.add_service, name="services-add"),
+    path('services/delete', views.delete_service, name="services-delete"),
+    path('services/edit', views.edit_service, name="services-edit"),    
+]
---
+++ .history/src/dashboard/views_20211124040305.py
@@ -0,0 +1,96 @@
+from django.shortcuts import render, redirect
+from django.views import View 
+from django.views.generic import ListView
+from users.models import User, DynamicEmailConfiguration, Service, ServicePackage, ServiceUser, ServiceUserSubscription
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
+# Create your views here.
+
+class IndexView(LoginRequiredMixin, View):
+    
+    """ Index View """  
+
+    login_url = '/login/'
+    redirect_field_name = 'redirect_to'
+    
+    def get(self, request):
+        
+        context = {
+        
+        }
+        return render(request, "dashboard/index.html", context=context)
+
+
+class EmailsView(View):
+    
+    def get(self, request):
+        all = DynamicEmailConfiguration.objects.all()
+        context = {
+            
+            "emails":all,
+            "count":all.count()
+            
+        }
+        
+        return render(request, 'dashboard/emails.html', context)
+    
+class UsersView(View):
+    
+    def get(self, request):
+        
+        all = User.objects.all()
+        context = {
+            
+            "users":all,
+            "count":all.count()
+            
+        }
+        
+        return render(request, 'dashboard/users.html', context)
+    
+class ServicesView(View):
+    
+    def get(self, request):
+        
+        context = {
+            "services":Service.objects.all()
+        }
+        
+        return render(request, 'dashboard/services.html')
+    
+    
+
+def login_view(request):
+    if request.POST:
+        
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            
+            # check if creds are true
+            data = form.cleaned_data
+            user = authenticate(email=data["email"], password=data["password"])
+            if user is not None:
+                user = User.objects.filter(email=data["email"]).first()
+                print(user)
+                login(request, user=user)
+                context = {"user":user}
+                messages.success(request, 'Login Successful Welcome to JAS, Janjas Auth System')
+                return redirect('dashboard')
+            
+        messages.error(request, "Username or Password is incorrect")
+        return redirect('login')
+    
+    context = {
+
+    }
+    return render(request, 'login_view.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Bye bye!')
+    return redirect('login')
+
