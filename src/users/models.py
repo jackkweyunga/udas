a50ce5e360629d8622a61b865298314024387df2
jackkweyunga <jackkweyunga@gmail.com>
2022-03-22 18:03:11 +0300

    users/models:

--- src/users/models.py
+++ src/users/models.py
@@ -5,7 +5,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.management.utils import get_random_secret_key
 
-from django.utils.translation import ugettext as _
 from django.core.exceptions import ValidationError
 from django.utils.text import slugify
 from django.core.management.utils import get_random_secret_key
@@ -75,9 +74,9 @@ class DynamicEmailConfiguration(models.Model):
     """
         Dynamic email configuration model
     """
-    email_name = models.CharField(unique=True, max_length=256, verbose_name = _("email unique name"))
+    email_name = models.CharField(unique=True, max_length=256, verbose_name = "email unique name")
     
-    slug = models.SlugField(verbose_name = _("slug"), unique=True, default=slugify(email_name))
+    slug = models.SlugField(verbose_name = "slug", unique=True, default=slugify(email_name))
     
     created_by = models.ForeignKey(User, related_name="emails", verbose_name="creator", on_delete=models.CASCADE, default=1)
     
@@ -85,52 +84,52 @@ class DynamicEmailConfiguration(models.Model):
     
     host = models.CharField(
         blank = True, null = True,
-        max_length = 256, verbose_name = _("Email Host"), default='smtp.gmail.com')
+        max_length = 256, verbose_name = "Email Host", default='smtp.gmail.com')
 
     port = models.SmallIntegerField(
         blank = True, null = True,
-        verbose_name = _("Email Port"), default=587)
+        verbose_name = "Email Port", default=587)
 
     from_email = models.CharField(
         blank = True, null = True,
-        max_length = 256, verbose_name = _("Default From Email"))
+        max_length = 256, verbose_name = "Default From Email")
 
     username = models.CharField(
         blank = True, null = True,
-        max_length = 256, verbose_name = _("Email Authentication Username"))
+        max_length = 256, verbose_name = "Email Authentication Username")
 
     password = models.CharField(
         blank = True, null = True,
-        max_length = 256, verbose_name = _("Email Authentication Password"))
+        max_length = 256, verbose_name = "Email Authentication Password")
 
     use_tls = models.BooleanField(
-        default = True, verbose_name = _("Use TLS"))
+        default = True, verbose_name = "Use TLS")
 
     use_ssl = models.BooleanField(
-        default = False, verbose_name = _("Use SSL"))
+        default = False, verbose_name = "Use SSL")
 
     fail_silently = models.BooleanField(
-        default = False, verbose_name = _("Fail Silently"))
+        default = False, verbose_name = "Fail Silently")
 
     timeout = models.SmallIntegerField(
         blank = True, null = True,
-        verbose_name = _("Email Send Timeout (seconds)"))
+        verbose_name = "Email Send Timeout (seconds)")
 
     def clean(self):
         if self.use_ssl and self.use_tls:
             raise ValidationError(
-                _("\"Use TLS\" and \"Use SSL\" are mutually exclusive, "
-                "so only set one of those settings to True."))
+                "\"Use TLS\" and \"Use SSL\" are mutually exclusive, "
+                "so only set one of those settings to True.")
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.email_name)
         super(DynamicEmailConfiguration, self).save(*args, **kwargs)
 
     def __str__(self):
-        return _(f"{self.slug} - {self.from_email}")
+        return f"{self.slug} - {self.from_email}"
 
     class Meta:
-        verbose_name = _("Email Configuration")
+        verbose_name = "Email Configuration"
 
 ####### services models
 
